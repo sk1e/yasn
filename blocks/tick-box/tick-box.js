ceec1b9e107daa4d37a9b7056e6945bb4f89117f
Sergey Petrov <sekk1e@yandex.ru>
2016-11-24 11:11:11 +0700

    + tick-box-field, fixed tick-box

--- blocks/tick-box/tick-box.js
+++ blocks/tick-box/tick-box.js
@@ -1,6 +1,13 @@
+const $ = require('jquery');
 
-$(function () {
-    $('.tick-box').on('click', function (event) {
-        $(this).toggleClass("tick-box_state_on tick-box_state_off")
-    })
-})
+$(() => {
+  $('.tick-box').on('click', function callback(event) {
+    $(this).toggleClass('tick-box_state_on tick-box_state_off');
+    const input = $(this).find('.tick-box__input')[0];
+
+    if (event.target !== input) {
+      // filters clicks on label, checkbox is already toggled if clicked on linked label
+      input.checked = !input.checked;
+    }
+  });
+});
