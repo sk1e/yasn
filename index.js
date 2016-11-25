9cc0a6099aca206615b0a76a82ba90cca38d8985
Sergey Petrov <sekk1e@yandex.ru>
2016-11-25 18:22:04 +0700

    + sign-up

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 
-require('./global.styl')
-require("./blocks/header/header.styl")
-require("./blocks/temptation/temptation.styl")
+require('./global.styl');
 
-require('./blocks/button/button.styl')
-require('./blocks/button/button.js')
-require('./blocks/tooltip/tooltip.styl')
 
-require('./fonts.styl')
+require('./blocks/link/link.styl');
+require('./blocks/tick-box-field/tick-box-field.styl');
+require('./blocks/sign-up/sign-up.js');
+require('./blocks/sign-up/sign-up.styl');
+require('./blocks/temptation/temptation.styl');
+require('./blocks/header/header.styl');
+
+require('./blocks/drop-down/drop-down.styl');
+require('./blocks/tick-box/tick-box.js');
+require('./blocks/tick-box/tick-box.styl');
+require('./blocks/input/input.styl');
+require('./blocks/stages/stages.styl');
+require('./blocks/button/button.styl');
+require('./blocks/button/button.js');
+require('./blocks/tooltip/tooltip.styl');
+
+require('./fonts.styl');
+require('./fonts/font-awesome-4.7.0/css/font-awesome.min.css');
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const webpack = require("webpack")
 
 module.exports = {
     entry: {
-        "ui-kit": "./ui-kit.js",
+        // "ui-kit": "./ui-kit.js",
         "index": "./index.js"
     },
     
@@ -22,19 +22,23 @@ module.exports = {
     
     module: {
         loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                include: [path.resolve(__dirname, 'blocks')],
+                query: { presets: ['babel-preset-latest']}
+            },
             { test: /\.pug$/, loader: 'pug' },
             { test: /\.json$/, loader: 'json' },
             { test: /\.(css|styl)/, loader: ExtractTextPlugin.extract('css!stylus') },
-            // { test: /\.(css|styl)/, loader: 'style!css!stylus' },
-            { test: /\.(svg|png|ttf|eot|woff|woff2)$/, loader: 'file?name=[path][name].[ext]' }
+             { test: /\.(svg|png|ttf|eot|woff|woff2)$/, loader: 'file?name=[path][name].[ext]' }
         ]
     },
 
     plugins: [
         // pp,
         new HtmlWebpackPlugin({template: "index.pug", filename: "index.html", chunks: ["index"]}),
-        // new HtmlWebpackPlugin({template: "index.pug", filename: "index.html", excludeChunks: ["ui-kit"]}),
-        new HtmlWebpackPlugin({template: "ui-kit.pug", filename: "ui-kit.html", chunks: ["ui-kit"]}),
+        // new HtmlWebpackPlugin({template: "ui-kit.pug", filename: "ui-kit.html", chunks: ["ui-kit"]}),
         new ExtractTextPlugin('[name].css', {allChunks: true}),
         new webpack.ProvidePlugin({
             $: 'jquery'
