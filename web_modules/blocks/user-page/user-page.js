aeb3d82df3f98ea92de10a1eede67ee81efe2edd
Sergey Petrov <sekk1e@yandex.ru>
2016-12-03 15:04:25 +0700

    + user page

---
+++ web_modules/blocks/user-page/user-page.js
@@ -0,0 +1,13 @@
+
+require('blocks/news/news');
+
+
+require('blocks/main-menu/main-menu');
+require('blocks/header/header');
+require('blocks/calendar/calendar');
+
+require('global.styl');
+require('fonts.styl');
+
+require('./user-page.styl');
+
