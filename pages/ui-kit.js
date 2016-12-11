b322cd856afedbd0b7a3973fa8441dacf7a2cb66
Sergey Petrov <sekk1e@yandex.ru>
2016-12-12 01:01:51 +0700

    fixed ui-kit

--- pages/ui-kit.js
+++ pages/ui-kit.js
@@ -1,26 +1,3 @@
-
 require('blocks/base-page/base-page');
-
-require('blocks/heading/heading');
-require('blocks/arrow-button/arrow-button');
-require('blocks/button/button');
-require('blocks/calendar/calendar');
-require('blocks/drop-down/drop-down');
-require('blocks/event/event');
-require('blocks/tooltiped-input/tooltiped-input');
-require('blocks/location/location');
-require('blocks/messenger/messenger');
-require('blocks/news/news');
-require('blocks/percentage/percentage');
-require('blocks/pie-chart/pie-chart');
-require('blocks/preview-column/preview-column');
-require('blocks/preview-row/preview-row');
-require('blocks/search/search');
-require('blocks/slider/slider');
-require('blocks/stages/stages');
-require('blocks/text-area/text-area');
-require('blocks/tick-box/tick-box');
-require('blocks/toggle/toggle');
-require('blocks/user-profile/user-profile');
-require('blocks/video/video');
+require('blocks/ui-kit-body/ui-kit-body');
 
