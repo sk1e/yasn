646bbda3d011c19911f617f4fe8788d80bf884e3
Sergey Petrov <sekk1e@yandex.ru>
2017-01-13 02:40:02 +0700

    fixed #30 (2)

--- web_modules/blocks/user-profile/index.js
+++ web_modules/blocks/user-profile/index.js
@@ -1,5 +1,27 @@
+import $ from 'jquery';
 import 'font-awesome.css';
 
 import '../tooltip';
 import '../user-photo';
 import './user-profile.styl';
+
+
+const UserProfile = class {
+  constructor($userProfile) {
+    this.$userProfile = $userProfile;
+  }
+
+  render() {
+    const $tooltip = this.$userProfile.find('.js-user-profile__tooltip');
+    const fontSize = parseInt($('html').css('font-size'), 10);
+    $tooltip.css('bottom', `calc(50% - ${$tooltip.height() / 2 / fontSize}rem)`);
+  }
+};
+
+
+$(() => {
+  $('.js-user-profile').each((_, node) => {
+    const userProfile = new UserProfile($(node));
+    userProfile.render();
+  });
+});
